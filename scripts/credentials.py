35f90cdad464fdca2b580b42f83ac29e736aed52
Francisco Seguí <83736724+fseguior@users.noreply.github.com>
2021-09-29 23:10:57 -0300

    Update credentials.py

--- scripts/credentials.py
+++ scripts/credentials.py
@@ -1,3 +1,3 @@
-client_ID='76547a2039e2480c8c86946d4889c701'       #Fill in with the value from the Spotify App
-client_SECRET='f1a041f027354b698a008d61ef5df20c'   #Fill in with the value from the Spotify App
+client_ID=''       #Fill in with the value from the Spotify App
+client_SECRET=''   #Fill in with the value from the Spotify App
 redirect_url='http://localhost:9000'
