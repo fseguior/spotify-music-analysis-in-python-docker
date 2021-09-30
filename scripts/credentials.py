8fee055e77b5d9fe76cdf118c3a057af536beead
Francisco Seguí <francisco.segui.or@gmail.com>
2021-09-29 23:12:28 -0300

    commit

--- scripts/credentials.py
+++ scripts/credentials.py
@@ -1,3 +1,3 @@
 client_ID='76547a2039e2480c8c86946d4889c701'       #Fill in with the value from the Spotify App
-client_SECRET='f1a041f027354b698a008d61ef5df20c'   #Fill in with the value from the Spotify App
+client_SECRET='082eeb49a67249a881da03021aee97a6'   #Fill in with the value from the Spotify App
 redirect_url='http://localhost:9000'
